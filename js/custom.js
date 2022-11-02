be5b26399fdfd42fdbe5b99903e3c502d819c1bb
swagata-codearts <swagata.codearts@gmail.com>
2022-11-02 17:10:58 +0530

    select icon updated

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,7 @@ jQuery('#testimonial-slider').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
     responsive:{
         0:{
             items:1
